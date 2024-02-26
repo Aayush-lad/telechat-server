fbe97a101b4aa77bb4e3e639d399bb460a379247
Aayush Lad <aayushlad1973@gmail.com>
2024-02-26 20:02:32 +0530

    Telechat version-1.0

---
+++ index.js
@@ -0,0 +1,196 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import AuthRoutes from './routes/AuthRoutes.js';
+import MessageRoutes from './routes/MessageRoutes.js';
+import { Server } from 'socket.io';
+import { Redis } from 'ioredis';
+import { produceMessage,startMessageConsumer } from './utils/kafka.js';
+
+
+dotenv.config();
+
+let prevMessage = undefined
+
+const app = express();
+const pub  = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+});
+
+
+const sub = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD
+})
+
+
+
+
+app.use("/uploads/images",express.static("uploads/images"))
+app.use("/uploads/audio",express.static("uploads/audio"))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/auth",AuthRoutes);
+app.use("/api/messages",MessageRoutes);
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log("Starting message consumer")
+    startMessageConsumer();
+    sub.subscribe("MESSAGES");    
+
+    
+});
+
+
+
+
+const io = new Server(
+    server,
+    {
+        cors: {
+            // origin: 'http://localhost:3000',
+            allowedHeaders:['*'],
+
+        }
+    }
+);
+global.onlineUsers= new Map();
+io.on('connection', (socket) => {
+
+ 
+    global.chatSocket = socket;
+    console.log('a user connected');
+
+
+    socket.on("add-user",(userId)=>{
+        onlineUsers.set(userId,socket.id);
+        console.log(global.onlineUsers);
+        socket.broadcast.emit("online-users",
+            Array.from(onlineUsers.keys())
+        )
+    })
+
+    socket.on("logout",(userId)=>{
+        onlineUsers.delete(userId);
+        socket.broadcast.emit("online-users",
+            Array.from(onlineUsers.keys())
+        )
+    })
+
+    socket.on("send-msg",async(data)=>{
+        console.log("publishing message");
+        
+        await pub.publish("MESSAGES",JSON.stringify({data})).then(console.log("Message sent to redis"))
+
+        // const sendUserSocket = onlineUsers.get(data.to);
+        // if(sendUserSocket){
+        //     socket.to(sendUserSocket).emit("msg-recieve",{
+        //         message:data.message,
+        //         from:data.from
+        //     })
+
+     await produceMessage(data);
+            
+        // }
+
+
+
+    sub.on("message", async (channel, message) => {
+        console.log("message received");
+
+        
+        if (channel === "MESSAGES" && prevMessage!==message) {
+            const data = JSON.parse(message);
+            console.log(data.data);
+    
+            const sendUserSocket = onlineUsers.get(data.data.to);
+            if (sendUserSocket) {
+                socket.to(sendUserSocket).emit("msg-recieve", {
+                    message: data.data.message,
+                    from: data.data.from
+                });
+
+                prevMessage = message;
+
+            
+                console.log("Message sent to kafka");
+            }
+        }
+    });
+
+
+
+
+        })
+
+  
+   
+    
+
+    socket.on("outgoing-voice-call",(data)=>{
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("incoming-voice-call",{
+                from:data.from,
+                callType:data.callType,
+                roomId:data.roomId
+            })
+        }
+    })
+
+    socket.on("outgoing-video-call",(data)=>{
+
+        console.log(data)
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("incoming-video-call",{
+                from:data.from,
+                callType:data.callType,
+                roomId:data.roomId
+            })
+        }
+    })
+
+    socket.on("reject-voice-call",(data)=>{
+        const sendUserSocket = onlineUsers.get(data.from);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("voice-call-rejected",{
+                from:data.from,
+                callType:data.callType,
+                roomId:data.roomId
+            })
+        }
+    })
+
+    socket.on("reject-video-call",(data)=>{
+        const sendUserSocket = onlineUsers.get(data.from);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("video-call-rejected",{
+                from:data.from,
+            })
+        }
+    })
+
+    socket.on("accept-incoming-call",({id})=>{
+
+        const sendUserSocket = onlineUsers.get(id);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("call-accepted");
+        }
+    })
+
+    
+});
+
+
+
+
+
---
+++ routes/MessageRoutes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { addMessage ,getMessages,addImageMessage,addAudioMessage, getInitialContactsWithMessage,searchMessages} from "../controllers/MessageController.js";
+import multer from "multer";
+
+
+
+const  uploadImage =multer({dest:"uploads/images"})
+const upload = multer({dest:"uploads/audio"}) 
+
+const router = Router();
+
+router.post("/add-message",addMessage);
+router.get("/get-messages/:from/:to",getMessages);
+router.post("/add-image-message",uploadImage.single("image"),addImageMessage);
+router.post("/add-audio-message",upload.single("audio"),addAudioMessage)
+router.get("/get-current-contacts/:from",getInitialContactsWithMessage)
+router.get("/search-messages/:from/:to/:search/:image/:audio/:message", searchMessages);
+
+
+
+
+export default router;
